da236c940ba715eb5943807106a49f1ba2f763b7
oday mardini <odaymard88@gmail.com>
2018-08-16 04:07:34 +0200

    Update telemetry.js

--- telemetry.js
+++ telemetry.js
@@ -1,8 +1,6 @@
 (function () {
   amp.plugin("telemetry", function (options) {
-
     window.onbeforeunload = function (e) { //sending the information when closimg the window
-
       fetch("/", {
         method: "POST",
         headers: {
@@ -15,16 +13,13 @@
           audiobitlog,
           texttracklog,
           events,
-          
         })
       });
 
     }
-
     let streamInformation = {}; //streaminfroamtion contains information about the languages and video and sound bit rate
     let videobitlog = {}, //this record the changes in the bitrate 
       audiobitlog = {};
-
     let events = {
       pause: {
         time: []
@@ -137,21 +132,11 @@
         };
       });
 
-      //   player.currentVideoStreamList().streams[0].tracks.forEach(function(element){
-      // videobitlog[element.bitrate]={download:0,failed:0,frames:0,changes:[]}
-      //     })
-
-      // player.textTracks().tracks_.map(element=>{return element.label})
       function evenLogHandler(e) {
-
-        console.log(e.type, player.currentTime());
-
         events[e.type].time.push(player.currentTime());
         console.log(e.type, "type");
         console.log("events", events);
-
       }
-
       player.addEventListener("play", evenLogHandler);
       player.addEventListener("pause", evenLogHandler);
       player.addEventListener("skip", evenLogHandler);
@@ -162,63 +147,14 @@
       player.addEventListener("error", evenLogHandler);
 
 
-      // player.addEventListener("play", function() {
-      //     console.log("play", player.currentTime());
-      //     events["play"].time.push(player.currentTime());
-      //     console.log("events", events);
-      // });
-
-      // player.addEventListener("pause", function() {
-      //     console.log("pause", player.currentTime());
-      //     events["pause"].time.push(player.currentTime());
-      //     console.log("events", events);
-      // });
-
-      // player.addEventListener("skip", function() {
-      //     console.log("skip", player.currentTime());
-      //     events["skip"].time.push(player.currentTime());
-      //     console.log("events", events);
-      // });
-
-      // player.addEventListener("waiting", function() {
-      //     console.log("waiting", player.currentTime());
-      //     events["buffering"].time.push(player.currentTime());
-      //     console.log("events", events);
-      // });
-
-      // player.addEventListener("fullscreenchange", function() {
-      //     console.log("fullscreenchange", player.currentTime());
-      //     events["fullscreenchange"].time.push(player.currentTime());
-      //     console.log("events", events);
-      // });
-
-      // player.addEventListener("volumechange", function() {
-      //     console.log("volumechange", player.currentTime());
-      //     events["volumechange"].time.push(player.currentTime());
-      //     console.log("events", events);
-      // });
-
-      // player.addEventListener("ended", function() {
-      //     console.log("ended", player.currentTime());
-      //     events["ended"].time.push(player.currentTime());
-      //     console.log("events", events);
-      // });
-
-      // player.addEventListener("error", function() {
-      //     console.log("error", player);
-      //     events["ended"].time.push(player.currentTime());
-      //     console.log("events", events);
-      // });
 
       player.addEventListener("texttrackchange", function () {
-        //   texttracklog[player.textTracks().currentTextTrack]
-        // console.log(player.getCurrentTextTrack().label , player.getCurrentTextTrack().mode)
+        
         if (player.getCurrentTextTrack() && player.getCurrentTextTrack().mode == "showing") {
           texttracklog[player.getCurrentTextTrack().label].changes.push(
             player.currentTime()
           );
           console.log("texttrackchanged", texttracklog);
-          //videobitlog[player.videoBufferData().downloadCompleted.mediaDownload.bitrate].changes.push(player.currentTime());
         }
       });
 
